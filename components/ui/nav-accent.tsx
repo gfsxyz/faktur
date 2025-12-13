e8eff9c7683b89152dbfaac8cb3b4764f2ba64b1
gfsxyz <gustifaizals@gmail.com>
2025-12-13 12:54:15 +0800

    style: add nav active accent & resize chart

--- components/ui/nav-accent.tsx
+++ components/ui/nav-accent.tsx
@@ -6,7 +6,13 @@ interface NavAccentProps {
 
 function NavAccent({ className }: NavAccentProps) {
   return (
-    <div className={cn("absolute top-1/2 -translate-y-1/2 -left-3", className)}>
+    <div
+      className={cn(
+        "absolute top-1/2 -translate-y-1/2 -left-3",
+        "animate-in slide-in-from-left-12 duration-300",
+        className
+      )}
+    >
       {/* Main bar */}
       <div className="relative h-7 w-1 bg-primary" />
       {/* Glow effect - on the right side of the bar extending into button */}
